1dba9335dec0d59e707fe386d434cc6cc8deeb22
thePianoKid <gbraden.dev@gmail.com>
2024-03-18 11:00:48 -0400

    Finished splash page

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,71 @@
-import Image from "next/image";
+"use client";
+import React from "react";
+import { HeroParallax } from "./components/ui/hero-parallax";
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+function HeroParallaxDemo() {
+  return <HeroParallax products={products} />;
 }
+export const products = [
+  {
+    title: "Robotic Artist",
+    link: "https://github.com/thePianoKid/steampunk-robotics-microPython",
+    thumbnail:
+      "https://www.notion.so/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2Ffdf9b984-61ae-4f61-9bbe-d1bb480f96b2%2F40dbb7c2-63c4-49a3-8ed6-531cd24da232%2Fev3.png?id=d0cf9111-2233-495f-a09a-1caa86c4798c&table=block&spaceId=fdf9b984-61ae-4f61-9bbe-d1bb480f96b2&width=2000&userId=c4b95cc2-6e5c-404e-89ab-6692222be8b3&cache=v2",
+  },
+
+  {
+    title: "Caesar’s Cipher",
+    link: "https://github.com/thePianoKid/cipher",
+    thumbnail:
+      "https://www.notion.so/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2Ffdf9b984-61ae-4f61-9bbe-d1bb480f96b2%2F684868c7-6884-435c-94db-a9b03e3e2f63%2Fcipher.png?table=block&id=0d8f05a8-db2e-4993-99b8-cab3a45f13cc&spaceId=fdf9b984-61ae-4f61-9bbe-d1bb480f96b2&width=2000&userId=c4b95cc2-6e5c-404e-89ab-6692222be8b3&cache=v2",
+  },
+
+  {
+    title: "React.js Blog with Google Authentication",
+    link: "https://steampunk-blog--development.gadget.app/",
+    thumbnail:
+      "https://www.notion.so/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2Ffdf9b984-61ae-4f61-9bbe-d1bb480f96b2%2F8a47d867-8da8-4f34-9920-2a0ce5405127%2FScreen_Shot_2024-03-18_at_10.43.00_AM.png?id=fb7b8e52-7ac5-4419-819b-be2fcb74a69c&table=block&spaceId=fdf9b984-61ae-4f61-9bbe-d1bb480f96b2&width=2000&userId=c4b95cc2-6e5c-404e-89ab-6692222be8b3&cache=v2",
+  },
+
+  {
+    title: "Conway's Game of Life",
+    link: "https://github.com/thePianoKid/Life-P5",
+    thumbnail:
+      "https://www.notion.so/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2Ffdf9b984-61ae-4f61-9bbe-d1bb480f96b2%2F8761ce33-7577-4ce0-89a4-1382e6cea203%2FScreen_Shot_2023-10-01_at_10.00.06_PM.png?table=block&id=40b67cfe-c70e-459e-b560-587b17d213f0&spaceId=fdf9b984-61ae-4f61-9bbe-d1bb480f96b2&width=2000&userId=c4b95cc2-6e5c-404e-89ab-6692222be8b3&cache=v2",
+  },
+  {
+    title: "Logomotive: A Review",
+    link: "https://www.linkedin.com/pulse/logomotive-review-gabe-braden-j4ejf/",
+    thumbnail:
+      "https://www.notion.so/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2Ffdf9b984-61ae-4f61-9bbe-d1bb480f96b2%2F7304ccc5-1ffa-46ba-baf2-98a92b0df277%2Flogo.png?id=cfe604ff-a76a-47a0-ad1d-9057762857f7&table=block&spaceId=fdf9b984-61ae-4f61-9bbe-d1bb480f96b2&width=2000&userId=c4b95cc2-6e5c-404e-89ab-6692222be8b3&cache=v2",
+  },
+
+  {
+    title: "Logomotive: A Review",
+    link: "https://www.linkedin.com/pulse/logomotive-review-gabe-braden-j4ejf/",
+    thumbnail:
+      "https://www.notion.so/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2Ffdf9b984-61ae-4f61-9bbe-d1bb480f96b2%2F7304ccc5-1ffa-46ba-baf2-98a92b0df277%2Flogo.png?id=cfe604ff-a76a-47a0-ad1d-9057762857f7&table=block&spaceId=fdf9b984-61ae-4f61-9bbe-d1bb480f96b2&width=2000&userId=c4b95cc2-6e5c-404e-89ab-6692222be8b3&cache=v2",
+  },
+
+  {
+    title: "Bug Squash",
+    link: "https://www.linkedin.com/posts/gabe-braden_i-had-such-a-great-time-working-on-this-project-activity-7137251233282564096-Po1u/?utm_source=share&utm_medium=member_desktop",
+    thumbnail:
+      "https://www.notion.so/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2Ffdf9b984-61ae-4f61-9bbe-d1bb480f96b2%2Fd88a723b-e812-4483-b63b-61d3630cda41%2FScreen_Shot_2024-03-18_at_10.46.56_AM.png?id=35d05844-5cbe-444b-86a2-053fedbb1917&table=block&spaceId=fdf9b984-61ae-4f61-9bbe-d1bb480f96b2&width=2000&userId=c4b95cc2-6e5c-404e-89ab-6692222be8b3&cache=v2",
+  },
+  {
+    title: "Steampunk Education",
+    link: "https://steampunk.education",
+    thumbnail:
+      "https://www.notion.so/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2Ffdf9b984-61ae-4f61-9bbe-d1bb480f96b2%2F9da2189d-48c1-437d-919a-e34b5b0ec178%2Fsteampunk4.png?id=86b73999-5436-4979-91b3-1d73704a1f16&table=block&spaceId=fdf9b984-61ae-4f61-9bbe-d1bb480f96b2&width=2000&userId=c4b95cc2-6e5c-404e-89ab-6692222be8b3&cache=v2",
+  },
+
+  {
+    title: "ReTell",
+    link: "https://userogue.com",
+    thumbnail:
+      "https://www.notion.so/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2Ffdf9b984-61ae-4f61-9bbe-d1bb480f96b2%2F28efbe42-0bac-414a-a3f1-fbc7a92a98a0%2Fretell.png?table=block&id=91e96a45-1a20-444a-8655-31f24b571802&spaceId=fdf9b984-61ae-4f61-9bbe-d1bb480f96b2&width=2000&userId=c4b95cc2-6e5c-404e-89ab-6692222be8b3&cache=v2",
+  },
+];
+
+export default HeroParallaxDemo;
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "aceternity.com",
+        port: "",
+        pathname: "/images/**",
+      },
+      {
+        protocol: "https",
+        hostname: "www.notion.so",
+        port: "",
+        pathname: "/image/**",
+      }
+    ],
+  },
+};
 
 export default nextConfig;
